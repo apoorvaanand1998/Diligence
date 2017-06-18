1572da6e23ff04cee7c5b3d27bb2fd615d98218a
Apoorva Anand <mitul.apoorva@gmail.com>
2017-06-18 13:47:36 +0530

    Add plotting - Date vs productive minutes

--- dil.py
+++ dil.py
@@ -1,13 +1,14 @@
-
 import sqlite3
+from datetime import date
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 conn = sqlite3.connect('diligence.db')
 
 c = conn.cursor()
 
-c.execute("CREATE TABLE IF NOT EXISTS diligence (date integer, prod real, worth real)")
-d = int(input("What's today's date? ")) 
+c.execute("CREATE TABLE IF NOT EXISTS diligence (date text, prod real, worth real)")
+d = input("What's today's date? ") 
 p = float(input("How many minutes did you spend doing something productive? "))
 w = float(input("How many minutes did you spend doing something worthless? "))
 c.execute("INSERT INTO diligence VALUES (?, ?, ?)", (d, p, w))
@@ -15,8 +16,17 @@ c.execute("INSERT INTO diligence VALUES (?, ?, ?)", (d, p, w))
 conn.commit()
 
 c.execute("SELECT date FROM diligence")
+res = []
 x = c.fetchall()
+for el in x:
+    for e in el:
+        res.append(date(year=int(e[0:4]), month=int(e[4:6]), day=int(e[6:8])))
+        
 c.execute("SELECT prod from diligence")
 y = c.fetchall()
-
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.plot(res, y)
+plt.gcf().autofmt_xdate()
+plt.show()
 
